68d54186710df19779a48659e199660e6b2e429b
Wendell <wendell230.olegario@gmail.com>
2024-02-13 19:20:50 -0300

    adição visual no nav bar e os cards

---
+++ dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request): 
+    return render(request,'dashboard/index.html')
+
+def staff(request):
+    return render(request,'dashboard/staff.html')
