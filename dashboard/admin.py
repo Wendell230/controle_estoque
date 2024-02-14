c449ed8b8a454cb11ab4734c1c4cfab0292758b1
Wendell <wendell230.olegario@gmail.com>
2024-02-14 15:47:45 -0300

    Criação dos models dos pedidos vinculando com FK

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 
@@ -11,4 +11,5 @@ class ProductAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Order)
 #admin.site.unregister(Group)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 CATEGORY = (
     ('papelaria','papelaria'),
@@ -12,5 +13,20 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices = CATEGORY , null=True)
     quantity = models.PositiveIntegerField(null=True)
     
+    class Meta:
+        verbose_name_plural = 'Produto'
+    
     def __str__(self):
         return f'{self.name} - {self.quantity}'
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Pedido'
+    
+    def __str__(self):
+        return f'{self.product} Encomendado por {self.staff.username}'
